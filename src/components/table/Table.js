0ce7488951b6caed42bf1c42c49619f5e6eb6d41
Isl0m <islomrustam09@gmail.com>
2023-04-26 20:58:20 +0500

    dom utils

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,3 +1,40 @@
 import { ExcelComponent } from '@core/ExcelComponent'
 
-export class Table extends ExcelComponent {}
+export class Table extends ExcelComponent {
+  static className = 'excel__table'
+  toHtml() {
+    return `
+    <div class="row">
+      <div class="row__info"></div>
+
+      <div class="row__data">
+        <div class="column">A</div>
+        <div class="column">B</div>
+        <div class="column">C</div>
+      </div>
+
+    </div>
+
+    <div class="row">
+      <div class="row__info">1</div>
+
+      <div class="row__data">
+        <div class="cell selected" contenteditable>sfdsd</div>
+        <div class="cell" contenteditable>sfdsdf</div>
+        <div class="cell" contenteditable>sdfsf</div>
+      </div>
+      
+    </div>
+    <div class="row">
+      <div class="row__info">2</div>
+
+      <div class="row__data">
+        <div class="cell" contenteditable>ssdffdsd</div>
+        <div class="cell" contenteditable>ssdffdsdf</div>
+        <div class="cell" contenteditable>sdfsfsf</div>
+      </div>
+      
+    </div>
+    `
+  }
+}
